529fcbbf14e4d1268e9cfe586d8dac00ebc8621b
Marina <marina.g.ivanova@gmail.com>
2016-10-23 22:11:46 +0300

    add index page + main front-end folders and files

---
+++ gulpfile.js
@@ -0,0 +1,133 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    assets  = require('postcss-assets'),
+    mqpacker = require('css-mqpacker'),
+    autoprefixer = require('autoprefixer'),
+    plumber = require('gulp-plumber'),
+    gulpif = require('gulp-if'),
+    sourcemaps = require('gulp-sourcemaps'),
+    fs = require('fs'),
+    loadPlugins  = require('gulp-load-plugins'),
+    $ = loadPlugins(),
+    del = require('del'),
+    connect = require('gulp-connect'),
+    config = {
+        appJsDir: 'app/js/',
+        appCssDir: 'app/scss/',
+        appImgDir: 'app/images/',
+        appFontDir: 'app/fonts/',
+        distJsDir: 'dist/js/',
+        distCssDir: 'dist/css/',
+        distImgDir: 'dist/images/',
+        distFontDir: 'dist/fonts/',
+        sourceMaps: false
+    };
+
+gulp.task('scripts', function() {
+    return gulp.src(config.appJsDir + '*.js')
+        .pipe(plumber())
+        .pipe(gulpif(config.sourceMaps, sourcemaps.init()))
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulpif(config.sourceMaps, sourcemaps.write()))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(config.distJsDir));
+});
+
+gulp.task('styles', function() {
+    return gulp.src(config.appCssDir + 'main.scss')
+        .pipe(plumber())
+        .pipe(gulpif(config.sourceMaps, sourcemaps.init()))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(postcss(
+            [
+                assets({
+                    relative: true,
+                    loadPaths: ['app/images','app/fonts'],
+                    cachebuster: function (filePath, urlPathname) {
+                        return fs.statSync(filePath).size.toString(36);
+                    }
+                }),
+                autoprefixer({
+                    remove: false,
+                    browsers: [ 'last 2 versions']
+                }),
+                mqpacker
+            ]
+        ))
+        .pipe(gulpif(config.sourceMaps, sourcemaps.write()))
+        .pipe(gulp.dest(config.distCssDir));
+});
+
+gulp.task('images', function(){
+    return gulp.src(config.appImgDir + '/**/*.+(png|jpg|jpeg|gif|svg)')
+        .pipe(gulp.dest(config.distImgDir))
+});
+
+gulp.task('fonts', function() {
+    return gulp.src(config.appFontDir  + '**/*.+(ttf|woff|eot|svg)')
+        .pipe(gulp.dest(config.distFontDir))
+});
+
+gulp.task('watch', function() {
+    gulp.watch(config.appJsDir + '**', ['scripts']);
+    gulp.watch(config.appCssDir + '**', ['styles']);
+    gulp.watch(config.appImgDir + '**', ['images']);
+    gulp.watch(config.appFontDir + '**', ['fonts']);
+});
+
+gulp.task('enableSourceMaps', function() {
+    config.sourceMaps = true;
+});
+
+gulp.task('svgsprite', function() {
+
+    var stream = gulp.src(config.appImgDir + '/sprite/*.svg')
+        .pipe($.svgmin({
+            plugins: [{
+                removeDoctype: true
+            },
+                {
+                    removeComments: true
+                }]
+        }))
+        .pipe($.svgstore({
+            inlineSvg: true
+        }))
+        .pipe($.rename('sprite.svg'))
+        .pipe($.cheerio({
+            run: function($) {
+                $('svg').attr({
+                    'width' : '0',
+                    'height' : '0',
+                    'display' : 'none',
+                    'aria-hidden' : 'true'
+                });
+            },
+            parserOptions: {
+                xmlMode: true
+            }
+        }))
+        .pipe(gulp.dest(config.distImgDir + 'sprite/'));
+
+    return stream;
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        livereload: true
+    });
+});
+
+gulp.task('html', function () {
+    gulp.src('./*.html')
+        .pipe(connect.reload());
+});
+
+gulp.task('debug', ['enableSourceMaps', 'default']);
+
+gulp.task('default', [ 'styles', 'scripts', 'fonts', 'images', 'watch', 'connect', 'html']);
